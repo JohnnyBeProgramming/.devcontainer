1ad5293a5413222cb7d70a23b668204ffbe2555e
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2024-03-09 11:18:10 +0100

    Add typer example

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+import typer
 
-print("Hello world!!")
+app = typer.Typer()
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__":
+    app()
